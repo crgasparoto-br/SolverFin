32bc3537224cd9fba0f86ca44b85f8e6a65933d1
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-06-16 11:37:33 -0300

    Expose transactions module

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -2,5 +2,6 @@ export * from "./app-shell/index.js";
 export * from "./dashboard/index.js";
 export * from "./design-system/index.js";
 export * from "./financial-catalog/index.js";
+export * from "./transactions/index.js";
 
 export const webAppName = "SolverFin Web";
