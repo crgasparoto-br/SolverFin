6ff04256a83e92a6a1c47eeceba12d5be5ca5f70
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-06-16 11:20:17 -0300

    Expose financial catalog module

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./app-shell/index.js";
 export * from "./dashboard/index.js";
 export * from "./design-system/index.js";
+export * from "./financial-catalog/index.js";
 
 export const webAppName = "SolverFin Web";
