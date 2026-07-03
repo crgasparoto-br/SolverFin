e171cefd68913b7ec96fe4936c4401837dd13d85
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-07-03 14:30:29 -0300

    Route accounts cards page through dialog-only renderer

--- apps/web/src/dev-server.ts
+++ apps/web/src/dev-server.ts
@@ -3,7 +3,7 @@ import { createServer, type IncomingMessage, type ServerResponse } from "node:ht
 import { buildSolverFinWebManifest } from "./pwa/manifest.js";
 import { renderAdminInstitutionsPage } from "./dev-server/admin-institutions-page.js";
 import { enhanceAccountsCardsTabs } from "./dev-server/accounts-cards-enhancement.js";
-import { renderAccountsCardsPage } from "./dev-server/accounts-cards-page.js";
+import { renderAccountsCardsPage } from "./dev-server/accounts-cards-page-dialog-only.js";
 import { handleApiRequest } from "./dev-server/api.js";
 import { renderCardsPage } from "./dev-server/cards-page.js";
 import { renderCategoriesPage } from "./dev-server/categories-page.js";
@@ -20,7 +20,7 @@ import { renderTransactionsPage } from "./dev-server/transactions-page.js";
 
 export { renderAdminInstitutionsPage } from "./dev-server/admin-institutions-page.js";
 export { enhanceAccountsCardsTabs } from "./dev-server/accounts-cards-enhancement.js";
-export { renderAccountsCardsPage } from "./dev-server/accounts-cards-page.js";
+export { renderAccountsCardsPage } from "./dev-server/accounts-cards-page-dialog-only.js";
 export { renderAccountsPage, renderBudgetsPage } from "./dev-server/pages.js";
 export { renderCardsPage } from "./dev-server/cards-page.js";
 export { renderCategoriesPage } from "./dev-server/categories-page.js";
