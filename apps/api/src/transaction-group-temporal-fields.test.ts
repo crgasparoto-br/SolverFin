a148139e4013be6ddc75795f6916bade38537e3c
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-08-18 08:37:57 -0300

    test: align group actions with temporal semantics

--- apps/api/src/transaction-group-temporal-fields.test.ts
+++ apps/api/src/transaction-group-temporal-fields.test.ts
@@ -5,6 +5,9 @@ import {
   resolveTransactionGroupUpdateTemporalFields,
 } from "./transaction-group-temporal-fields.js";
 
+const update = resolveTransactionGroupUpdateTemporalFields;
+const clone = resolveTransactionGroupCloneTemporalFields;
+
 const posted = {
   status: "POSTED",
   occurredOn: "2034-01-10",
@@ -12,34 +15,21 @@ const posted = {
   effectiveOn: "2034-03-20",
 };
 
-assert.deepEqual(
-  resolveTransactionGroupUpdateTemporalFields(posted, {
-    plannedOn: "2034-02-22",
-  }),
-  {
-    occurredOn: "2034-01-10",
-    plannedOn: "2034-02-22",
-    effectiveOn: "2034-03-20",
-  },
-);
-assert.deepEqual(
-  resolveTransactionGroupUpdateTemporalFields(posted, {
-    effectiveOn: "2034-03-25",
-  }),
-  {
-    occurredOn: "2034-01-10",
-    plannedOn: "2034-02-15",
-    effectiveOn: "2034-03-25",
-  },
-);
-assert.deepEqual(
-  resolveTransactionGroupUpdateTemporalFields(posted, { date: "2034-03-27" }),
-  {
-    occurredOn: "2034-01-10",
-    plannedOn: "2034-02-15",
-    effectiveOn: "2034-03-27",
-  },
-);
+assert.deepEqual(update(posted, { plannedOn: "2034-02-22" }), {
+  occurredOn: "2034-01-10",
+  plannedOn: "2034-02-22",
+  effectiveOn: "2034-03-20",
+});
+assert.deepEqual(update(posted, { effectiveOn: "2034-03-25" }), {
+  occurredOn: "2034-01-10",
+  plannedOn: "2034-02-15",
+  effectiveOn: "2034-03-25",
+});
+assert.deepEqual(update(posted, { date: "2034-03-27" }), {
+  occurredOn: "2034-01-10",
+  plannedOn: "2034-02-15",
+  effectiveOn: "2034-03-27",
+});
 
 const planned = {
   status: "PLANNED",
@@ -47,25 +37,19 @@ const planned = {
   plannedOn: "2034-02-16",
   effectiveOn: null,
 };
-assert.deepEqual(
-  resolveTransactionGroupUpdateTemporalFields(planned, { date: "2034-02-23" }),
-  {
-    occurredOn: "2034-01-11",
-    plannedOn: "2034-02-23",
-    effectiveOn: null,
-  },
-);
+assert.deepEqual(update(planned, { date: "2034-02-23" }), {
+  occurredOn: "2034-01-11",
+  plannedOn: "2034-02-23",
+  effectiveOn: null,
+});
 
-assert.deepEqual(resolveTransactionGroupCloneTemporalFields(posted, {}), {
+assert.deepEqual(clone(posted, {}), {
   occurredOn: "2034-01-10",
   plannedOn: "2034-02-15",
   effectiveOn: "2034-03-20",
 });
-assert.deepEqual(
-  resolveTransactionGroupCloneTemporalFields(posted, { date: "2035-04-05" }),
-  {
-    occurredOn: "2035-04-05",
-    plannedOn: "2035-04-05",
-    effectiveOn: "2035-04-05",
-  },
-);
+assert.deepEqual(clone(posted, { date: "2035-04-05" }), {
+  occurredOn: "2035-04-05",
+  plannedOn: "2035-04-05",
+  effectiveOn: "2035-04-05",
+});
